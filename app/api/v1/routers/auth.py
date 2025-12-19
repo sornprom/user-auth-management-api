0d79d990009b69d55befb982e205f824ff0e40d1
Sornprom <sornprom1004@gmail.com>
2025-12-19 21:32:51 +0700

    chore: change response message

--- app/api/v1/routers/auth.py
+++ app/api/v1/routers/auth.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas.user import UserCreate
 from app.services.auth_service import AuthService
 from app.core.database import AsyncSessionLocal
-
+from fastapi.responses import JSONResponse
 router = APIRouter(prefix="/auth", tags=["Auth"])
 service = AuthService()
 
@@ -15,7 +15,10 @@ async def get_db():
 async def register(payload: UserCreate, db: AsyncSession = Depends(get_db)):
     existing_user = await service.repo.get_by_email(db, payload.email)
     if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        return JSONResponse(
+            status_code=400,
+            content={"message": "Email already registered"}
+        )
     await service.register(db, payload.email, payload.password)
     return { "message": "Resregister success"}
 
@@ -23,5 +26,8 @@ async def register(payload: UserCreate, db: AsyncSession = Depends(get_db)):
 async def login(payload: UserCreate, db: AsyncSession = Depends(get_db)):
     token = await service.login(db, payload.email, payload.password)
     if not token:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        return JSONResponse(
+            status_code=401,
+            content={"message": "Invalid email or password"}
+        )
     return token
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -3,11 +3,3 @@ from pydantic import BaseModel, EmailStr
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
-
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    is_active: bool
-
-    class Config:
-        from_attributes = True
