13ee58d3704dd7359a5837777bd8761bbbae6641
Sornprom <sornprom1004@gmail.com>
2025-12-19 21:19:11 +0700

    chore: edit message register success

--- app/api/v1/routers/auth.py
+++ app/api/v1/routers/auth.py
@@ -16,7 +16,8 @@ async def register(payload: UserCreate, db: AsyncSession = Depends(get_db)):
     existing_user = await service.repo.get_by_email(db, payload.email)
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return await service.register(db, payload.email, payload.password)
+    await service.register(db, payload.email, payload.password)
+    return { "message": "Resregister success"}
 
 @router.post("/login")
 async def login(payload: UserCreate, db: AsyncSession = Depends(get_db)):
